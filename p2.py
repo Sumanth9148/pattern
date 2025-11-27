7cf974da562539dc27c8d1fec92c49211928f4c6
Sumanth9148 <sumanthrn93@gmail.com>
2025-11-27 12:00:46 +0530

    new files

---
+++ p2.py
@@ -0,0 +1,30 @@
+#pyramid
+# n=5
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(" ",end="")
+
+#     for k in range(i+1):
+#         print("*",end=" ")
+#     print()        
+
+
+#diamond
+#upper part
+n=5
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+
+    for k in range(i+1):
+        print("*",end=" ")
+    print()
+
+#lower part
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end="")
+
+    for k in range(n-i-1):
+        print("*",end=" ")
+    print()
---
+++ p3.py
@@ -0,0 +1,25 @@
+#numbers 1
+# n=5
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i,end="")
+#     print()    
+
+
+#numbers 2
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()    
+
+
+#numbers 3
+n=5
+sum=1
+for i in range(n):
+    for j in range(i+1):
+        print(sum,end=" ")
+        sum+=1
+
+    print()    
